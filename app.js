b82a2c115181012be7417969e1f87f8c612f6ae9
Magali L. Espinosa <90401660+MagaliL@users.noreply.github.com>
2025-08-03 22:35:28 -0600

    Add files via upload
    Subir archivos

---
+++ app.js
@@ -0,0 +1,14 @@
+
+let numeroSecreto= 6;
+let numeroUsuario = prompt("Me indicas un número, por favor: ");
+
+// Una buena práctica es poner las variables, la descripción junta
+
+console.log(numeroUsuario);
+
+if (numeroUsuario == numeroSecreto ) {
+    alert("Acertaste en el número secreto")
+
+}
+
+

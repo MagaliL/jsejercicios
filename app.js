e993a2f3daf43dde5875c70812774dcc11918c4f
Magali L. Espinosa <magalizethe@gmail.com>
2025-08-04 13:15:58 -0600

    Se agregó un mensaje de error al ejercicio 1

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
-
+//Variables
 let numeroSecreto= 6;
-let numeroUsuario = prompt("Me indicas un número, por favor: ");
+let numeroUsuario = prompt("Me indicas un número entre 1 y 10, por favor: ");
 
 // Una buena práctica es poner las variables, la descripción junta
 
 console.log(numeroUsuario);
 
+/*Este bloque de código es una condición para comparar los números que dio el usuario y el número secreto que ya se encuentra almacenado en la variable numeroSecreto*/
+
 if (numeroUsuario == numeroSecreto ) {
     alert("Acertaste en el número secreto")
 
+}else {
+    alert("Lo sentimos, tu número no es correcto.")
 }
 
 

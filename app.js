5e76da398602d25bbd396b8f9e52de81f834c1a0
Magali L. Espinosa <magalizethe@gmail.com>
2025-08-07 17:47:27 -0600

    Se agregó un mensaje que dice al usuario cuántos intentos ha tenido mediante un contador en el bloque de código que indica que el usuario ha ingresado un número erróneo

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 //Variables
 let numeroSecreto= 6;
 let numeroUsuario = 0; /*-> Aunque no se está utilizando aquí y se le va a pedir al usuario que dé el valor de esta variable, es necesario declararla y asignarle valor antes de pasarla como parámetro al bucle while.*/
+let intentos = 1;
+let palabraIntento ="intento";
+
 
 while(numeroUsuario != numeroSecreto){
 
-    let numeroUsuario = prompt("Me indicas un número entre 1 y 10, por favor: ");
+    numeroUsuario = prompt("Me indicas un número entre 1 y 10, por favor: ");
 
     // Una buena práctica es poner las variables, la descripción junta
 
@@ -13,19 +16,29 @@ while(numeroUsuario != numeroSecreto){
     /*Este bloque de código es una condición para comparar los números que dio el usuario y el número secreto que ya se encuentra almacenado en la varia5ble numeroSecreto*/
 
     if (numeroUsuario == numeroSecreto ) {
-        alert(`Acertaste, el número secreto es: ${numeroSecreto}`)
+        alert(`Acertaste, el número secreto es: ${numeroSecreto}. Lo hiciste en ${intentos} ${palabraIntento}.`);
+
+
         //Se cumple la condición
 
     }else {
         if(numeroUsuario > numeroSecreto)
         {
             alert("El número esperado es menor");
-        }else{
+        }else  //No se cumple la condición
+            
+            {
             alert("El número esperado es mayor");
         }
 
+        //Contador directo. Está colocado en el bloque del else porque se implementa cuando la persona no acierta. 
+
+        intentos = intentos+1;
+        palabraIntento = "intentos";
+        //Se vuelve a asignar el valor intentos en la variable palabraIntento pues la variable intentos ahora no es solamente igual a 1. Si la redefinimos aquí va a volverse a ejecutar el ciclo y palabraIntento será plural.
+
         // alert("Lo sentimos, tu número no es correcto.")
-        //No se cumple la condición
+       
     }
 }; 
 

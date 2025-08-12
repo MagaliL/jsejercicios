b230f16c937078157bbc8f80532febf29bee11a1
Magali L. Espinosa <magalizethe@gmail.com>
2025-08-12 14:09:22 -0600

    Se hizo que el programa creará de forma aleatoria el número secreto con el método random y que se convirtiera la entrada del usuario a tipo de dato number para que no se tuviera errores más adelante

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
 //Variables
-let numeroSecreto= 6;
+let numeroSecreto= Math.floor(Math.random()*10)+1;
 let numeroUsuario = 0; /*-> Aunque no se está utilizando aquí y se le va a pedir al usuario que dé el valor de esta variable, es necesario declararla y asignarle valor antes de pasarla como parámetro al bucle while.*/
 let intentos = 1;
 // let palabraIntento ="intento";
 let maxIntentos= 3;
 
 
+// Console es para prueba
+// console.log(`El número secreto es: ${numeroSecreto}`);
+
+
 while(numeroUsuario != numeroSecreto){
 
-    numeroUsuario = prompt("Me indicas un número entre 1 y 10, por favor: ");
+    numeroUsuario = parseInt(prompt("Me indicas un número entre 1 y 10, por favor: "));
 
     // Una buena práctica es poner las variables, la descripción junta
 
-    console.log(numeroUsuario);
+    console.log(`El número que ingresaste es ${numeroUsuario} y su tipo de dato es: ${typeof(numeroUsuario)}`);
+    //Este console log es para que me vaya indicando en consola los números que va ingresando el usuario. Cuando se le pone typeof(variable) es para sacar el tipo de dato de la variable, en este caso va a sacar el tipo de dato de lo que va a ingresar el usuario.  
 
     /*Este bloque de código es una condición para comparar los números que dio el usuario y el número secreto que ya se encuentra almacenado en la varia5ble numeroSecreto*/
 
